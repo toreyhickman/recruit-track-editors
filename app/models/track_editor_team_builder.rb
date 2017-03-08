b88ff843d1cdf442b261184f7f97ec45de7d876b
Torey Hickman <torey@toreyhickman.com>
2017-03-08 15:46:09 -0500

    Creates track editor teams

---
+++ app/models/track_editor_team_builder.rb
@@ -0,0 +1,48 @@
+class TrackEditorTeamBuilder
+  TRACK_EDITOR_TEAM_NAME_MATCHER = /\Atrack\-editors\-for/
+
+  def initialize(org_name, client = GithubClient)
+    @org_name = org_name
+    @client   = client
+  end
+
+
+  def build_teams
+    client.fetch_teams(org_name).each_with_object([]) do |team_data, track_editor_teams|
+      next unless for_track_editor_team?(team_data)
+
+      track_editor_teams << build_team(team_data)
+    end
+  end
+
+  private
+  attr_reader :org_name, :client
+
+  def for_track_editor_team?(team_data)
+    extract_name(team_data) =~ TRACK_EDITOR_TEAM_NAME_MATCHER
+  end
+
+  def build_team(team_data)
+    TrackEditorTeam.new({
+      :name         => extract_name(team_data),
+      :id           => extract_id(team_data),
+      :member_names => get_member_names(extract_id(team_data))
+    })
+  end
+
+  def extract_name(team_data)
+    team_data["name"]
+  end
+
+  def extract_id(team_data)
+    team_data["id"]
+  end
+
+  def get_member_names(team_id)
+    client.fetch_members(team_id).map { |member_data| extract_member_name(member_data) }
+  end
+
+  def extract_member_name(member_data)
+    member_data["login"]
+  end
+end
---
+++ spec/models/track_editor_team_builder_spec.rb
@@ -0,0 +1,37 @@
+require_relative "../spec_helper"
+
+describe TrackEditorTeamBuilder do
+  let(:client) { double("client") }
+  let(:org_name) { "some-org" }
+  let(:builder) { TrackEditorTeamBuilder.new(org_name, client) }
+
+  let(:track_editor_teams) { builder.build_teams }
+
+  describe "building teams" do
+    before(:each) do
+      allow(client).to receive(:fetch_teams).with(org_name).and_return(JSON.parse(File.read("#{APP_ROOT}/spec/fixtures/request_teams_response.json")))
+      allow(client).to receive(:fetch_members).with(2222222).and_return(JSON.parse(File.read("#{APP_ROOT}/spec/fixtures/request_team_members_response.json")))
+    end
+
+
+    it "ignores non-track-editor teams" do
+      expect(track_editor_teams.length).to eq 1
+    end
+
+    describe "building a track editor team" do
+      let(:team) { track_editor_teams.first }
+
+      it "builds a team with a name" do
+        expect(team.name).to eq "track-editors-for-something"
+      end
+
+      it "builds a team with an id" do
+        expect(team.id).to eq 2222222
+      end
+
+      it "builds a team with member names" do
+        expect(team.member_names).to match_array ["teacher", "instructor"]
+      end
+    end
+  end
+end
