33bc155069ce617546d0a16a5186951812da6869
Torey Hickman <torey@toreyhickman.com>
2017-03-09 11:49:47 -0500

    Loads .env file in application root

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,12 @@
-require "dotenv"
-Dotenv.load
-
 require "pathname"
+require "dotenv"
 require "httparty"
 require "slack_ruby_client"
 
 APP_ROOT = Pathname.new(File.expand_path("../../", __FILE__))
 APP_NAME = APP_ROOT.basename.to_s
+
+Dotenv.load("#{APP_ROOT}/.env")
 
 Dir[APP_ROOT.join("app/models/**/*.rb")].each { |file| require file }
 
